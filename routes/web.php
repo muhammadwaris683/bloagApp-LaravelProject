8087d35f16393f60c9dbda5f264498fd8e9cd72d
Muhammad Waris <wariali4204@gmail.com>
2024-09-18 14:53:32 +0500

    second commit

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,4 @@ Route::resource('posts', PostController::class);
 
 
 
-// Route::get('/posts', [PostController::class, 'index']);           // Show all posts
-// Route::get('/posts/create', [PostController::class, 'create']);   // Form to create a post
-// Route::post('/posts', [PostController::class, 'store']);          // Store new post
-// Route::get('/posts/{id}', [PostController::class, 'show']);       // Show a single post
-// Route::get('/posts/{id}/edit', [PostController::class, 'edit']);  // Show form to edit a post (GET /posts/{id}/edit)
-// Route::put('/posts/{id}', [PostController::class, 'update']);
-// Route::delete('/posts/{id}', [PostController::class, 'destroy']); // Delete a post
+
